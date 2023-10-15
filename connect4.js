74edce49a207f853e42f4dbd4ed2a77c4f0b56e5
Jiahao Li <jiahaoli@jiahaos-mbp.dyn.brandeis.edu>
2023-10-14 20:06:57 -0400

    added falling animation but buggy, will be fix

--- connect4.js
+++ connect4.js
@@ -1,17 +1,18 @@
 
 
 
-let p1 = "pink"
-let p2 = "orange"
-window.onload = function() {
-  let game = new Game(p1, p2);
-  document.getElementById("widgetRestartGame").addEventListener("click", () => game.playAgain());
-  document.getElementById("widgetResetGame").addEventListener("click", () => game.resetPlayers());
+
+// window.onload = function() {
+//   let p1 = "pink"
+//   let p2 = "orange"
+//   let game = new Game(p1, p2);
+//   document.getElementById("widgetRestartGame").addEventListener("click", () => game.playAgain());
+//   document.getElementById("widgetResetGame").addEventListener("click", () => game.resetPlayers());
 
 
-  // makeBoard();
-  // makeDropper();
-}
+//   // makeBoard();
+//   // makeDropper();
+// }
 
 
 
@@ -102,17 +103,31 @@ class Game {
     if (r < 0) {
         return;
     }
+    
     let tile = document.getElementById(r.toString() + "-" + d.toString());
-
     if (this.currPlayer == this.player1) {
         tile.classList.add("onePiece");
         tile.style.backgroundColor = this.player1;
+        tile.classList.add('fall');
+        
+
+
+
+
         this.board[r][d] = this.player1;
     } else {
         tile.classList.add("twoPiece");
         tile.style.backgroundColor = this.player2;
+        tile.classList.add('fall');
+
+
+
+
+
         this.board[r][d] = this.player2;
     }
+
+    
 
     if (this.checkWinner()) { // If there's a winner, set gameOver to true and return
         this.gameOver = true;
@@ -126,6 +141,8 @@ class Game {
     } else {
         this.currPlayer = this.player1;
     }
+    document.documentElement.style.setProperty('--current-player-color', this.currPlayer);
+
 
     r -= 1;
     this.currentCol[d] = r;
@@ -233,35 +250,142 @@ clearDropper() {
 
 
 
-// const formElement = document.querySelector("form");
-// const playerName1 = document.querySelector("#playerName1");
-// const playerName2 = document.querySelector("#playerName2");
-// const color1 = document.querySelector("#p1color");
-// const color2 = document.querySelector("#p2color");
+const formElement = document.querySelector("form");
+const playerName1 = document.querySelector("#playerName1");
+const playerName2 = document.querySelector("#playerName2");
+const color1 = document.querySelector("#p1color");
+const color2 = document.querySelector("#p2color");
+let p1color;
+let p2color;
+// let game = new Game(null, null);
+formElement.addEventListener("submit", function(e) {
+  e.preventDefault();
+  document.getElementById("board").innerHTML = '';
+  document.getElementById("dropper").innerHTML = '';
 
-// formElement.addEventListener("submit", function(e) {
-//   e.preventDefault();
+  p1color = color1.value.trim();
+  p2color = color2.value.trim();
+  if (!isValidColor(p1color)) {
+    p1color = 'red';  // default color for player 1
+  }
+  if (!isValidColor(p2color)) {
+    p2color = 'blue'; // default color for player 2
+  }
+  
+  let game = new Game(p1color, p2color);
+  // game.player1 = p1color;
+  // game.player2 = p2color;
+  document.documentElement.style.setProperty('--current-player-color', p1color);
 
-//   let p1color = color1.value.trim();
-//   let p2color = color2.value.trim();
+  document.getElementById("widgetRestartGame").addEventListener("click", () => game.playAgain());
+  document.getElementById("widgetResetGame").addEventListener("click", () => game.resetPlayers());
 
-//   // Check if entered colors are valid
-//   if (!isValidColor(p1color)) {
-//     p1color = 'red';  // default color for player 1
+
+});
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  const p1Tile = document.querySelector('#p1tile');
+  p1Tile.style.backgroundColor = 'red';
+
+  const p2Tile = document.querySelector('#p2tile');
+  p2Tile.style.backgroundColor = 'blue';
+
+  document.documentElement.style.setProperty('--current-player-color', p1color);
+
+});
+
+
+
+//=============
+// const input1 = document.querySelector('#p1color');
+// const input2 = document.querySelector('#p2color');
+
+const commonColors = [
+  'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 
+  'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 
+  'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 
+  'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 
+  'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 
+  'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 
+  'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 
+  'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'GoldenRod', 'Gray', 'Grey', 'Green', 
+  'GreenYellow', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 
+  'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 
+  'LightGoldenRodYellow', 'LightGray', 'LightGrey', 'LightGreen', 'LightPink', 'LightSalmon', 
+  'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 
+  'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 
+  'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 
+  'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 
+  'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 
+  'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 
+  'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 
+  'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 
+  'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 
+  'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 
+  'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'
+];
+
+// input1.addEventListener('keyup', searchHandler(input1));
+// input2.addEventListener('keyup', searchHandler(input2));
+
+
+// function searchHandler(input) {
+// 	let tempResults = search(input.value);  // Pass the value of the input
+
+//     showSuggestions(tempResults, input.value);
+
+//     // TODO: Logic to handle user input and display matching suggestions.
+
+
+// }
+
+// function search(str) {
+   
+
+// 	let results = colors.filter((item)=> item.toLowerCase().includes(str.toLowerCase()));
+//     // TODO: Logic for filtering the fruit list based on the search term.
+
+//     return results;
+// }
+
+// function showSuggestions(results, inputVal) {
+//   // suggestions.innerHTML = ''; // Clear previous suggestions
+//   if (results.length === 0 || inputVal.length === 0) {
+//       return; // No suggestions to show
 //   }
-//   if (!isValidColor(p2color)) {
-//     p2color = 'blue'; // default color for player 2
-//   }
 
-//   const p1name = playerName1.value.trim();
-//   const p2name = playerName2.value.trim();
+//   results.sort((a, b) => {
+//       const indexA = a.toLowerCase().indexOf(inputVal.toLowerCase());
+//       const indexB = b.toLowerCase().indexOf(inputVal.toLowerCase());
+//       return indexA - indexB;
+//   });
 
-//   if (p1name && p2name) {
-//     const player1 = new Player(p1name, p1color);
-//     const player2 = new Player(p2name, p2color);
-//     const game = new Game(player1, player2);
+  
 
-//     console.log("hello world");
+// }
+
+
+// //===================
+
+// // const p1Tile = document.querySelector('#p1tile');
+// //   p1Tile.style.backgroundColor = 'red';
+
+// //   const p2Tile = document.querySelector('#p2tile');
+// //   p2Tile.style.backgroundColor = 'blue';
+
+// //   // Check if entered colors are valid
+ 
+
+// //   const p1name = playerName1.value.trim();
+// //   const p2name = playerName2.value.trim();
+
+// //   if (p1name && p2name) {
+// //     const player1 = new Player(p1name, p1color);
+// //     const player2 = new Player(p2name, p2color);
+// //     const game = new Game(player1, player2);
+
+// //     console.log("hello world");
 
 
 
@@ -269,16 +393,14 @@ clearDropper() {
 
     
 
-//     // Hide initial screen and show game screen
-//     // document.getElementById('initialScreen').style.display = 'none';
-//     // document.getElementById('gameScreen').style.display = 'flex';
+// //     // Hide initial screen and show game screen
+// //     // document.getElementById('initialScreen').style.display = 'none';
+// //     // document.getElementById('gameScreen').style.display = 'flex';
 
-//   } else {
-//     alert("Please fill in the player names before starting the game!");
-//   }
-//   // Game code
-
-
+// //   } else {
+// //     alert("Please fill in the player names before starting the game!");
+// //   }
+// //   // Game code
 
 
 
@@ -291,11 +413,34 @@ clearDropper() {
 
 
 
-// });
 
-// function isValidColor(color) {
-//   const s = new Option().style;
-//   s.color = color;
-//   // return 'false' if color wasn't assigned
-//   return s.color !== '';
-// }
+
+// // });
+function isValidColor(strColor) {
+  var s = new Option().style;
+  s.color = strColor;
+  return s.color !== '' || commonColors.includes(strColor);
+}
+
+
+color1.addEventListener('input', function() {
+  const currentColor = isValidColor(color1.value.trim()) ? color1.value.trim() : null;
+  if (currentColor) {
+      document.querySelector('#p1tile').style.backgroundColor = currentColor;
+  }
+  else {
+    document.querySelector('#p1tile').style.backgroundColor = 'red';
+
+  }
+});
+
+color2.addEventListener('input', function() {
+  const currentColor = isValidColor(color2.value.trim()) ? color2.value.trim() : null;
+  if (currentColor) {
+      document.querySelector('#p2tile').style.backgroundColor = currentColor;
+  }
+  else {
+    document.querySelector('#p2tile').style.backgroundColor = 'blue';
+
+  }
+});
